dd4409081b7c2ce7a58f7cbe235a8e4876ca6fa6
aryanruthwik24 <aryanruthwik24@gmail.com>
2025-06-10 20:29:39 +0530

    update

--- Frontend/src/components/Loader.jsx
+++ Frontend/src/components/Loader.jsx
@@ -1,24 +1,25 @@
-import React from 'react';
-
 export default function Loader() {
   return (
     <div
       className="loader"
       style={{
-        position: 'fixed', // covers the entire screen
+        position: 'fixed', 
         top: 0,
         left: 0,
         width: '100vw',
         height: '100vh',
-        backgroundColor: 'rgba(0,0,0,0.5)', // semi-transparent dark overlay
+        backgroundColor: 'rgba(0,0,0,0.5)', 
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        zIndex: 9999, // high enough to be on top of everything
+        flexDirection:'column',
+        zIndex: 9999, 
       }}
     >
       <div className="spinner-border text-light" role="status">
-        <span className="sr-only">Loading...</span>
+      </div>
+      <div style={{color:'white'}}>
+      Loading...
       </div>
     </div>
   );
